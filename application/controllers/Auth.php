3ef48fe5581ec916f72704276cb4e4ba667df1b0
sauqi28 <riza.valasev@peruri.co.id>
2023-04-09 10:17:48 +0700

    TEST

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -4,7 +4,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Auth extends CI_Controller
 {
-
+  //TEST GIT
   public function __construct()
   {
     parent::__construct();

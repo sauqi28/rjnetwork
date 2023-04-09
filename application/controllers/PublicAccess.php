3707dc37f9734db672cf9535278b8325687565f2
sauqi28 <riza.valasev@peruri.co.id>
2023-04-09 11:16:13 +0700

    publicaccess

--- application/controllers/PublicAccess.php
+++ application/controllers/PublicAccess.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class PublicAccess extends CI_Controller
+class Publicaccess extends CI_Controller
 {
   public function __construct()
   {

0d26ffe152bc74d9090972025fd836087725a8ee
sauqi28 <riza.valasev@peruri.co.id>
2023-04-09 11:00:34 +0700

    footer

--- application/views/project/element/footer1.php
+++ application/views/project/element/footer1.php
@@ -1,5 +1,5 @@
     <footer class="footer text-center text-sm-start">
       &copy; <script>
         document.write(new Date().getFullYear())
-      </script> PT PLN (Persero) <span class="text-muted d-none d-sm-inline-block float-end">by </i> CV. Antares Sintesa Digital</span>
+      </script> PT PLN (Persero) <span class="text-muted d-none d-sm-inline-block float-end">by </i> Antares Sintesa Digital</span>
     </footer>

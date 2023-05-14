35573ad6c71d9f5b19730e066301822fb4eb28e3
sauqi28 <riza.valasev@peruri.co.id>
2023-05-14 14:26:57 +0700

    s

--- application/views/data/monitor/navbar.php
+++ application/views/data/monitor/navbar.php
@@ -1,8 +1,8 @@
   <ul class="navbar-nav me-auto mb-2 mb-lg-0">
     <li class="nav-item ">
-      <a class="nav-link mdi mdi-format-list-bulleted-type <?php if ($navbar == "monitor/index") {
+      <a class="nav-link mdi mdi-format-list-bulleted-type <?php if ($navbar == "monitor/users") {
                                                               echo "active";
-                                                            } ?>" href="<?php echo base_url('monitor/index'); ?>">Daftar User</a>
+                                                            } ?>" href="<?php echo base_url('monitor/users'); ?>">Daftar User</a>
     </li>
     <li class="nav-item ">
       <a class="nav-link mdi mdi-format-list-bulleted-type <?php if ($navbar == "monitor/nonaktif") {

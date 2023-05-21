fec0dc69adc56e8f999dc751289f08d9907c574a
sauqi28 <riza.valasev@peruri.co.id>
2023-05-21 08:21:24 +0700

    a

--- application/views/data/monitor/navbar.php
+++ application/views/data/monitor/navbar.php
@@ -2,12 +2,17 @@
     <li class="nav-item ">
       <a class="nav-link mdi mdi-format-list-bulleted-type <?php if ($navbar == "monitor/users") {
                                                               echo "active";
-                                                            } ?>" href="<?php echo base_url('monitor/users'); ?>">Daftar User</a>
+                                                            } ?>" href="<?php echo base_url('monitor/users'); ?>">Daftar Semua User</a>
     </li>
     <li class="nav-item ">
       <a class="nav-link mdi mdi-format-list-bulleted-type <?php if ($navbar == "monitor/nonaktif") {
                                                               echo "active";
                                                             } ?>" href="<?php echo base_url('monitor/nonaktif'); ?>">Daftar User Non Aktif</a>
+    </li>
+    <li class="nav-item ">
+      <a class="nav-link mdi mdi-format-list-bulleted-type <?php if ($navbar == "monitor/isolir") {
+                                                              echo "active";
+                                                            } ?>" href="<?php echo base_url('monitor/nonaktif'); ?>">Daftar User Isolir</a>
     </li>
 
 
